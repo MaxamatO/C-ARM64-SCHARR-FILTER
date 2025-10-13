d9bcf9cce068bb12459027fcad561200b99a892c
maks <pierchala.maks@gmail.com>
2025-10-13 17:24:22 +0200

    Initial Commit

---
+++ ScharrFilter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ScharrFilter
+//
+//  Created by Maksymilian Pierchała on 13/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ScharrFilter/ScharrFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScharrFilterApp.swift
+//  ScharrFilter
+//
+//  Created by Maksymilian Pierchała on 13/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct ScharrFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
