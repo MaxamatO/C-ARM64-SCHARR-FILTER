ad351e7a44d0c120ddc41c67c5323210fea903d7
maks <pierchala.maks@gmail.com>
2025-10-28 17:51:26 +0100

    Scharr filter single thread

--- ScharrFilter/c/Cscharr.c
+++ ScharrFilter/c/Cscharr.c
@@ -60,49 +60,63 @@ void calculateColumns(uint8_t *p_greyScalePixels, int16_t *p_yScharrPixels, int 
             int indexUpperRow = (y-1)*width + x;
             int indexLowerRow = (y+1)*width + x;
             int scharrValue =
-                g_kernelDx[0][0]*p_greyScalePixels[indexUpperRow-1] +
-                g_kernelDx[0][1]*p_greyScalePixels[indexUpperRow] +
-                g_kernelDx[0][2]*p_greyScalePixels[indexUpperRow+1] +
-                g_kernelDx[2][0]*p_greyScalePixels[indexLowerRow-1] +
-                g_kernelDx[2][1]*p_greyScalePixels[indexLowerRow] +
-                g_kernelDx[2][2]*p_greyScalePixels[indexLowerRow+1];
+                g_kernelDy[0][0]*p_greyScalePixels[indexUpperRow-1] +
+                g_kernelDy[0][1]*p_greyScalePixels[indexUpperRow] +
+                g_kernelDy[0][2]*p_greyScalePixels[indexUpperRow+1] +
+                g_kernelDy[2][0]*p_greyScalePixels[indexLowerRow-1] +
+                g_kernelDy[2][1]*p_greyScalePixels[indexLowerRow] +
+                g_kernelDy[2][2]*p_greyScalePixels[indexLowerRow+1];
             p_yScharrPixels[y*width + x] = (int16_t)scharrValue;
         }
     }
 }
 
 void combinePixels(int16_t *p_xScharrPixels, int16_t *p_yScharrPixels, uint8_t *p_combinedPixels, int width, int height){
-    int combinedValue;
+    double combinedValue;
+    int combinedFinalValue;
     uint8_t finalValue;
     for (int y=0; y < height; y++) {
         for (int x=0; x < width; x++) {
             int index = y*width + x;
             int16_t xCurrentPixel = p_xScharrPixels[index];
             int16_t yCurrentPixel = p_yScharrPixels[index];
-            combinedValue = sqrt(xCurrentPixel*xCurrentPixel + yCurrentPixel*yCurrentPixel);
-            if (combinedValue >= 255){
+            double x_sqrt = xCurrentPixel*xCurrentPixel;
+            double y_sqrt = yCurrentPixel*yCurrentPixel;
+            combinedValue = sqrt(x_sqrt + y_sqrt);
+            combinedFinalValue = (int) combinedValue;
+            if (combinedFinalValue >= 255){
                 finalValue = 255;
+            }else {
+                finalValue = (uint8_t)combinedFinalValue;
             }
-            finalValue = (uint8_t)combinedValue;
             p_combinedPixels[index] = finalValue;
+        }
+    }
+}
+
+void expandToRGBA(uint8_t *p_combinedPixels, uint8_t *p_scharrFilteredPixels, int width, int height, int bytesPerRow){
+    for (int y=0; y < height; y++) {
+        for (int x=0; x < width; x++) {
+            int singleByteIndex = y*width + x;
+            int fourByteIndex = y*bytesPerRow + x*4;
+            p_scharrFilteredPixels[fourByteIndex] = p_combinedPixels[singleByteIndex];
+            p_scharrFilteredPixels[fourByteIndex+1] = p_combinedPixels[singleByteIndex];
+            p_scharrFilteredPixels[fourByteIndex+2] = p_combinedPixels[singleByteIndex];
+            p_scharrFilteredPixels[fourByteIndex+3] = 255;
         }
     }
 }
 
 
 
-//TODO: malloc pamiec na 1bajtowy greyscale, operacje na tym arrayu - scharr. potem rozszerzamy z powrotem do 4 bajtowego RGBA - szary, A=255
-//TODO: zwalniac pamiec odpowiednio
-
-
-
 void convertToGreyScale(uint8_t *p_originalPixels, uint8_t *destination,int width, int height, int bytesPerRow){
     for (int y = 0; y < height; y++) {
         uint8_t *row = p_originalPixels + y * bytesPerRow;
-        uint8_t *greyScaleRow = destination + y*bytesPerRow;
         for (int x = 0; x < width; x++) {
-            int i = x * 4;
-            greyScaleRow[i] = row[i+0]*0.299 + row[i+1]*0.587 + row[i+2]*0.114;
+            int fourByteIndex = x * 4;
+            int singleByteIndex =y*width + x;
+            int greyValue = (row[fourByteIndex + 0] * 77 + row[fourByteIndex + 1] * 150 + row[fourByteIndex + 2] * 29) >> 8;
+            destination[singleByteIndex] = (uint8_t)greyValue;
         }
     }
 }
@@ -119,14 +133,24 @@ struct CProcessedData processImage(uint8_t *p_pixels, int width, int height, int
     int16_t *p_yScharrPixels = (int16_t*)malloc(greyScaleBytesPerRow*height*sizeof(int16_t));
     calculateColumns(p_greyScalePixels,p_yScharrPixels, width, height, bytesPerRow);
 
-    int16_t *p_combinedPixels = (int16_t*)malloc(greyScaleBytesPerRow*height*sizeof(int16_t));
+    
+    uint8_t *p_combinedPixels = (uint8_t*)malloc(greyScaleBytesPerRow*height);
+    combinePixels(p_xScharrPixels, p_yScharrPixels, p_combinedPixels, width, height);
+    
+    uint8_t *p_scharrFilteredPixels = (uint8_t*)malloc(height*bytesPerRow);
+    expandToRGBA(p_combinedPixels, p_scharrFilteredPixels, width, height, bytesPerRow);
+    
     struct CProcessedData processedData = {
-        .data = p_greyScalePixels,
+        .data = p_scharrFilteredPixels,
         .height = height,
         .width = width,
         .bytesPerRow = bytesPerRow,
         .bitsPerComponent = 8
     };
+    free(p_greyScalePixels);
+    free(p_xScharrPixels);
+    free(p_yScharrPixels);
+    free(p_combinedPixels);
     
     printf("C: processedData ptr=%p %dx%d\n", processedData.data, processedData.width, processedData.height);
     return processedData;
--- ScharrFilter/c/ScharrFilter-Bridging-Header.h
+++ ScharrFilter/c/ScharrFilter-Bridging-Header.h
@@ -14,3 +14,4 @@ void convertToGreyScale(uint8_t *p_originalPixels, uint8_t *destination,int widt
 void calculateRows(uint8_t *p_greyScalePixels, int16_t *p_xScharrPixels, int width, int height, int bytesPerRow);
 void calculateColumns(uint8_t *p_greyScalePixels, int16_t *p_yScharrPixels, int width, int height, int bytesPerRow);
 void combinePixels(int16_t *p_xScharrPixels, int16_t *p_yScharrPixels, uint8_t *p_combinedPixels, int width, int height);
+void expandToRGBA(uint8_t *p_combinedPixels, uint8_t *p_scharrFilteredPixels,int width, int height, int bytesPerRow);
